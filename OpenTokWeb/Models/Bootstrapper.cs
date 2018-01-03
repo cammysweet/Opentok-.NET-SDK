662e340fbd94cbaf94be85894e3fd0267af0a02a
Camila Cardoso <camila.cardoso65@hotmail.com>
2018-01-03 14:36:32 -0200

    web nancy

---
+++ OpenTokWeb/Models/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace OpenTokWeb.Models
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            container.Register<OpenTokService>().AsSingleton();
+        }
+    }
+}
